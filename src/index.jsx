d07e50c6895cf58b6e46bd917b5d1ddd0683207b
Kyle Kelley <rgbkrk@gmail.com>
2015-12-15 16:24:04 -0600

    Wire up a react-redux Provider component.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
 
 import App from './components/App';
 import Voting from './components/Voting';
@@ -8,12 +11,25 @@ import Results from './components/Results';
 
 require('./style.css');
 
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: { Sunshine: 2 },
+    },
+  },
+});
+
 const routes = <Route component={App}>
   <Route path='/results' component={Results} />
   <Route path='/' component={Voting} />
 </Route>;
 
 ReactDOM.render(
-  <Router>{routes}</Router>,
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
