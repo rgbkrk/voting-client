39fa4859c9d01b4ee2e0c87ca43514c538d36fa7
Kyle Kelley <rgbkrk@gmail.com>
2015-12-15 09:03:46 -0600

    buttons should be disabled when user has voted

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -31,4 +31,16 @@ describe('Voting', () => {
     expect(votedWith).to.equal('Trainspotting');
   });
 
+  it('disables buttons when user has voted', () => {
+    const component = renderIntoDocument(
+      <Voting pair={['Trainspotting', '28 Days Later']}
+              hasVoted='Trainspotting' />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+  });
+
 });
