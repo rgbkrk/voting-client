5749ee5673e7a3fbe8f3cd0f3d6ee0250d3f25e8
Kyle Kelley <rgbkrk@gmail.com>
2015-12-15 08:39:13 -0600

    Test vote click.

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,6 +4,7 @@ export default React.createClass({
   displayName: 'Voting',
   propTypes: {
     pair: React.PropTypes.array,
+    vote: React.PropTypes.func,
   },
   getPair() {
     return this.props.pair || [];
@@ -11,7 +12,8 @@ export default React.createClass({
   render() {
     return <div className='voting'>
       { this.getPair().map(entry =>
-        <button key={entry}>
+        <button key={entry}
+                onClick={() => this.props.vote(entry)} >
           <h1>{entry}</h1>
         </button>
       )}
--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -2,7 +2,7 @@ import React from 'react/addons';
 import Voting from '../../src/components/Voting';
 import { expect } from 'chai';
 
-const { renderIntoDocument, scryRenderedDOMComponentsWithTag } = React.addons.TestUtils;
+const { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } = React.addons.TestUtils;
 
 describe('Voting', () => {
 
@@ -15,6 +15,20 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Trainspotting');
     expect(buttons[1].textContent).to.equal('28 Days Later');
+  });
+
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+
+    const component = renderIntoDocument(
+      <Voting pair={ ['Trainspotting', '28 Days Later'] }
+              vote={vote}/>
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+
+    expect(votedWith).to.equal('Trainspotting');
   });
 
 });
