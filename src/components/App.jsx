edef6f67735b4d423237c24b4507eb8ae066954e
Kyle Kelley <rgbkrk@gmail.com>
2015-12-15 11:42:35 -0600

    Introduce ReactRouter into the mix.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { List } from 'immutable';
+
+const pair = List.of('Trainspotting', '28 Days Later');
+
+export default React.createClass({
+  displayName: 'App',
+  propTypes: {
+    children: React.PropTypes.node,
+  },
+  render() {
+    return React.cloneElement(this.props.children, { pair });
+  },
+});
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
 
 require('./style.css');
 
-const pair = ['Trainspotting', '28 Days Later'];
+const routes = <Route component={App}>
+  <Route path='/' component={Voting} />
+</Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} winner='Trainspotting' />,
+  <Router>{routes}</Router>,
   document.getElementById('app')
 );
