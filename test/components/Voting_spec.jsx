8fdbba7ea6f873f9b1714cf35e4ff71a61b37e59
Kyle Kelley <rgbkrk@gmail.com>
2015-12-15 09:09:44 -0600

    Render only winner when there is one

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -53,4 +53,16 @@ describe('Voting', () => {
     expect(buttons[0].textContent).to.contain('Voted');
   });
 
+  it('renders just the winner when there is one', () => {
+    const component = renderIntoDocument(
+      <Voting winner='Trainspotting' />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    expect(buttons.length).to.equal(0);
+
+    const winner = React.findDOMNode(component.refs.winner);
+    expect(winner).to.be.ok;
+    expect(winner.textContent).to.contain('Trainspotting');
+  });
+
 });
