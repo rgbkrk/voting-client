a48f161d2cb2e329195b7e4ff2e5cb7a6ab71409
Kyle Kelley <rgbkrk@gmail.com>
2015-12-15 08:50:06 -0600

    Messy bits of voting and winning.

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -3,18 +3,32 @@ import React from 'react';
 export default React.createClass({
   displayName: 'Voting',
   propTypes: {
+    hasVoted: React.PropTypes.bool,
     pair: React.PropTypes.array,
     vote: React.PropTypes.func,
+    winner: React.PropTypes.string,
   },
   getPair() {
     return this.props.pair || [];
   },
+  isDisabled() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  },
   render() {
     return <div className='voting'>
-      { this.getPair().map(entry =>
+      {this.props.winner ?
+        <div ref='winner'>Winner is {this.props.winner}!</div> :
+        this.getPair().map(entry =>
         <button key={entry}
+                disabled={this.isDisabled()}
                 onClick={() => this.props.vote(entry)} >
           <h1>{entry}</h1>
+          {this.hasVotedFor(entry) ?
+            <div className='label'>Voted</div> :
+          null}
         </button>
       )}
     </div>;
